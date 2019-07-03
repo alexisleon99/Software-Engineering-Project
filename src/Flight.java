999c68d7a2c386d7a3c2e7c5c874361ec143d6d4
alexisleon99 <alexisleon99@gmail.com>
2019-07-03 14:53:05 -0400

    trying to merege to my branch

--- src/Flight.java
+++ src/Flight.java
@@ -1,4 +1,18 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Flight {
+	String flightNum;
+	String originAirport;
+	String destinationAriport;
+	boolean isFull;
+	double potentialValue;
+	double actualValue;
+	LocalDate departureDate;
+	LocalTime departureTime;
+	
+	public String getFlightNum() {
+		return flightNum;
+	}
 
 }
