4ffe4dcc8c04ec2d3fd74cf4633b5dd7b4e05295
alexisleon99 <alexisleon99@gmail.com>
2019-07-08 20:59:06 -0400

    Added line and javadoc
    added a line to show seat types to users and added some javadocs

--- src/Flight.java
+++ src/Flight.java
@@ -3,19 +3,51 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
-
+/**
+ * This class implements a simple flight object which is used to search and 
+ * find flights of diffrent airlines.
+ * @author alexis
+ * @version Sprint_1
+ */
 public class Flight {
 	String airline;
 	String fname;
 	String ssn;
 	double budget;
-
+/**
+ * This constructs the Flight object with the airline, first name, SSN, and budget.
+ * @param airline This is the airline from which the flight is being chosen from.
+ * @param fname this is the first name of the person who wishes to take the flight.
+ * @param ssn This is the social security number of the person.
+ * @param budget This is the budget that the person has.
+ */
 	public Flight(String airline, String fname,String ssn, double budget) {
 		this.airline = airline;
 		this.fname = fname;
 		this.ssn = ssn;	
 		this.budget = budget;
 	}
+	/**
+	 * <p>
+	 * This search method allows the person to search the different flights
+	 * The person will first be asked to which airline they wish to search for.
+	 * </p>
+	 * The results are returned depending on which airline they choose either delta or jetblue.
+	 * If the person asks for an airline that is not part of the airport a message will be printed
+	 * out stating this fact.
+	 * <p>
+	 * once the person has choosen an airline and looked at the available flight, they will be asked to 
+	 * make a reservation or just say that they are looking around.
+	 * </p>
+	 * <p>
+	 * If person chooses to make a reservation, the search method will create an object of reservation 
+	 * using the airline, first name, ssn, budget, and the map data structure for tickets
+	 * </p>
+	 * @see {@link Reservation}
+	 * 
+	 * 
+	 * @throws IOException Used in case of the an IO error and is handled to allow program form crashing.
+	 */
 	public void search() throws IOException {
 
 		Scanner input = new Scanner(System.in);
@@ -40,7 +72,6 @@ public class Flight {
 		}else {
 			System.out.println("Airline not with airport");
 		}
-		
 		System.out.println("Will you like to purchase a reservation to one of these destinations?");
 		System.out.println("1 - YES Reserve" + "\n2 - NO Just looking around");
 		
--- src/Seat.java
+++ src/Seat.java
@@ -1,19 +1,39 @@
 import java.io.FileNotFoundException;
 import java.util.Random;
 import java.util.Scanner;
-
+/**
+ * This implements a object of Seat type, which is used to choose the seat type when purchasing a 
+ * flight
+ * @author Alexis Leon
+ * @see {@link Reservation}
+ */
 public class Seat {
 	double cost;
 	String visit;
-	
+	/**
+	 * This constructor is used in order to create the seat object.
+	 * @param cost This is the cost of the flight chosen.
+	 * @param visit this is the destination of the flight.
+	 */
 	public Seat(double cost, String visit){
 		this.cost = cost;
 		this.visit = visit;
 	}
 	Scanner seat = new Scanner(System.in);
-	
+	/**
+	 * The seatClass method will ask the person when making a reservation which type of seat they 
+	 * would like. 
+	 * <p>
+	 * The person can choose between First, Business, or Economy class.
+	 * </p>
+	 * @param cost This is the cost of the flight that was chosen by the person to the certain
+	 * Destination.
+	 * @param visit This is the destination that was chosen.
+	 * @throws FileNotFoundException
+	 */
 	public void SeatClass(Double cost, String visit) throws FileNotFoundException{
 		System.out.println("Please choose what type of seat you will Like");
+		System.out.println("First" + "/t" + "Business" +"/t" + "Economy");
 		String type = seat.next();
 		if(type.equals("First")) {
 			cost = cost * 2;
@@ -21,11 +41,19 @@ public class Seat {
 		}else if(type.equals("Business")) {
 			SeatChart(cost,visit);
 			cost = cost * 1.5;
-		}else if(type.equals("Economoy")) {
+		}else if(type.equals("Economy")) {
 			SeatChart(cost,visit);			
 		}
 	}
-	
+	/**
+	 * This method will be used in order to assign a randomized row and seat letter.
+	 * @param cost This is the cost of the flight.
+	 * @param visit This is the destination of the flight.
+	 * <p>
+	 * once the row and seat letter has been randomized it will pass the 
+	 * </p>
+	 * @throws FileNotFoundException
+	 */
 	public void SeatChart(double cost,String visit) throws FileNotFoundException {
 		Random r = new Random();
 		int letter = r.nextInt(90 - 65) + 65;
