10f245de96fd9514fe7fdcc3dad1bd2b2f4a2096
Jose Delgado <delgadoj23418@gmail.com>
2019-07-09 20:26:20 -0400

    Changed junit for Flight class

--- src/FlightTest.java
+++ src/FlightTest.java
@@ -8,30 +8,16 @@ import org.junit.jupiter.api.Test;
 class FlightTest {
 
 	@Test
-	void testFlight() {
-		String Airline = "Delta";
-		String fname = "Alexis";
-		String ssn = "84812789";
-		//flight test = new Flight();
-		
-		
-	}
-
-	@Test
 	void testSearch() throws IOException {
-		Flight flight = new Flight("delta", null, null, 0); 
+		Flight flight = new Flight("delta", null, null, 10000.0); 
 		HashMap <String,Double> expected = new HashMap();
 		expected.put("Austin", (double) 3500);
 		expected.put("Portland", (double) 4500);
 		expected.put("Chicago", (double) 3800);
 		expected.put("Miami", (double) 3200);
 		expected.put("New York", (double) 3000);
-		expected.put("Arizona", (double) 5000);	
-		
+		expected.put("Arizona", (double) 5000);			
 		HashMap actual = flight.search("delta");
-		assertSame(flight,actual);
-		
-
+		assertEquals(expected,actual);
 	}
-
 }
