b0316232db8fcd4dd082156fa971cf58408b8c6a
alexisleon99 <alexisleon99@gmail.com>
2019-07-09 21:50:24 -0400

    trying to get unit test to work
    little progress made :(

--- src/FlightTest.java
+++ src/FlightTest.java
@@ -1,23 +1,41 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 import org.junit.jupiter.api.Test;
 
 class FlightTest {
-
+	Flight expectedTest = new Flight("Delta","Alexis","84812789",10000.00);
 	@Test
 	void testFlight() {
+		
 		String Airline = "Delta";
 		String fname = "Alexis";
 		String ssn = "84812789";
-		double budget = 10000;
+		double budget = 10000.00;
 		Flight test = new Flight(Airline,fname,ssn,budget);
-
-		//flight test = new Flight();
+		System.out.println(test.equals(expectedTest));
+		assertTrue(test.equals(test));
 	}
-
 	@Test
 	void testSearch() {
-		fail("Not yet implemented");
+		try {
+			HashMap <String,Double> map2 = new HashMap();
+			map2.put("Austin", (double) 3500);
+			System.out.println("please enter Austin for the location");
+			System.out.println(map2.entrySet());
+			expectedTest.search();
+			//System.out.println(map.entrySet());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
+
+
+
 
 }
