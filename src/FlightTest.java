7c62d56112e5b80ac86e373f10d3907fe1b5971b
alexisleon99 <alexisleon99@gmail.com>
2019-07-29 23:08:01 -0400

    saving in order to pull down another branch

--- src/FlightTest.java
+++ src/FlightTest.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.HashMap;
@@ -10,13 +11,28 @@ class FlightTest {
 
 	@Test
 	void testlists() throws IOException, ParseException {
-		Flight p1 = new Flight("delta", "alex","123456789",10000);
-		String airline = "delta", answer = "yes";
+		String airline = "delta", answer = "yes",name = "alex",ssn = "222";
 		double budget = 10000,cost =7000; 
+		Flight p1 = new Flight(airline,name,ssn,budget);
 		HashMap locations = new HashMap();
 		p1.addCompanion(airline,budget,locations,answer,cost);
+		findFile("reservation2221.txt",src);
 		//lists("delta");
-		
 	}
-
+    public void findFile(String name,File file)
+    {
+        File[] list = file.listFiles();
+        if(list!=null)
+        for (File fil : list)
+        {
+            if (fil.isDirectory())
+            {
+                findFile(name,fil);
+            }
+            else if (name.equalsIgnoreCase(fil.getName()))
+            {
+                System.out.println(fil.getParentFile());
+            }
+        }
+    }
 }
